1f794adc638a2549a200381d5bedd90de8683bc9
Chris Kay <chriskay25@gmail.com>
2019-07-15 15:29:02 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,4 +3,16 @@ CREATE TABLE projects (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date )
+  start_date TEXT,
+  end_date TEXT);
+  
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER);
