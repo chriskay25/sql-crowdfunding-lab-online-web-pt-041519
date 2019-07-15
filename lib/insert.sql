7c8abfa2784e8f3d56fd48d6ac227585bc5ddb1e
Chris Kay <chriskay25@gmail.com>
2019-07-15 15:45:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,29 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("project_1", "math", 1000, "01-01-2020", "02-01-2020");
 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_2", "math", 2000, "01-15-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_3", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_4", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_5", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_6", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_7", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_8", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_9", "math", 1000, "01-01-2020", "02-01-2020");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project_10", "math", 1000, "01-01-2020", "02-01-2020");
